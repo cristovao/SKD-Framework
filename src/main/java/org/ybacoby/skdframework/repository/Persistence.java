c7d484f06ae70a4a52241e46091abf0f1bf7c8b5
cristovao wollieson <cristovao@cristovao-laptop.(none)>
2010-02-03 08:46:43 -0300

    Usando a interface persistence para adicionar os objetos no banco

---
+++ src/main/java/org/ybacoby/skdframework/repository/Persistence.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.ybacoby.skdframework.repository;
+
+/**
+ *
+ * @author cristovao
+ */
+public interface Persistence {
+
+}
